42f160a0176f43db9638626b272d855120356358
Brown <JENIFER.A.BROWN@saic.com>
2020-10-06 14:44:18 -0400

    checkin

---
+++ sidebars.js
@@ -0,0 +1,20 @@
+module.exports = {
+  someSidebar: {
+    Sage: [
+       {
+         type: 'category',
+         label: 'Sage Users Guide',
+         items: [
+           'sage-intro',
+           'sage-cloud-app-mod-overview',
+           'sage-getting-started',
+           'sage-managing-apps',
+           'sage-troubleshooting'
+         ]
+       }
+       ],
+    // Sage: ['sage-user-guide'],
+    Docusaurus: ['doc1', 'doc2', 'doc3'],
+    Features: ['mdx'],
+  },
+};
